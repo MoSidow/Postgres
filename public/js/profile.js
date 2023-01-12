288c994d707f26b9aafb808abe6e1ea3fd037e0d
Ahmed Alasow <a.alasow01@gmail.com>
2023-01-12 20:42:52 +0000

    Added javascript code for the login, logout and profile pages

---
+++ public/js/profile.js
@@ -0,0 +1,48 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const productName = document.querySelector('#product-name').value.trim();
+    const productPrice = document.querySelector('#product-price').value.trim();
+    const description = document.querySelector('#product-desc').value.trim();
+  
+    if (productName && productPrice && description) {
+      const response = await fetch(`/api/products`, {
+        method: 'POST',
+        body: JSON.stringify({ productName, productPrice, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create product');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/products/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete this product');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-jewellery-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.jewellery-list')
+    .addEventListener('click', delButtonHandler);
+  
