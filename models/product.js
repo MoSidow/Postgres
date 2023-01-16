473efa25dbf3386c87e06dd8cecf1151ba6bcea3
Ting Wong <ting@Tings-MacBook-Air.local>
2023-01-16 18:50:40 +0000

    changed productData datatype to TEXT, updated title to name

--- models/product.js
+++ models/product.js
@@ -16,7 +16,7 @@ Product.init(
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
     },
     date_created: {
       type: DataTypes.DATE,
